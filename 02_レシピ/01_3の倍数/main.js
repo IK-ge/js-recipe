5764ecbb1daf7f16db0a1d7246a5294997efc2c2
IK-ge <ig2267454@gmail.com>
2022-10-16 13:29:33 +0900

    [クイズ（1問）]を学習しました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -2,8 +2,12 @@
 const genkiFunction = function (number) {
   //nを1から、numberまで、値を1ずつ増やしながら、forの{}の中身を繰り返す
   for (let n = 1; n <= number; n++) {
-    if (n % 3 === 0) {
-      console.log(`${n}!!!!!!!`)
+    if (n % 15 === 0) {
+      console.log("FizzBuzz")
+    } else if (n % 3 === 0) {
+      console.log("Fizz")
+    } else if (n % 5 === 0) {
+      console.log("Buzz")
     } else {
       console.log(n)
     }
